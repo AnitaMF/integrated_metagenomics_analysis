de50b9edeaf3a6079fa62dc13e59f919618c2154
Ana Mejia F <anamejiafleisacher@gmail.com>
2024-07-01 11:49:43 +0300

    pytest

--- test_functions.py
+++ test_functions.py
@@ -27,7 +27,7 @@ def test_process_breport_files():
 
     df = pd.read_csv(output_file, index_col=0)
     assert "Ruminiclostridium cellulolyticum" in df.index
-    assert df.loc["Ruminiclostridium cellulolyticum", df.columns[0]] == 107
+    assert df.loc["Ruminiclostridium cellulolyticum", df.columns[0]] == 57
 
 def test_list_csv_files():
     expected_files = ['S_counts_matrix.csv']  
@@ -52,7 +52,7 @@ def test_compute_relative_abundance():
     print("DataFrame:")
     print(df.head()) 
     assert "Ruminiclostridium cellulolyticum" in df.index
-    assert df.loc["Ruminiclostridium cellulolyticum", df.columns[0]] == 0.0020903475564227
+    assert df.loc["Ruminiclostridium cellulolyticum", df.columns[0]] == 0.0020013806014956
 
 
 def test_filter_low_abundance_species():
@@ -71,7 +71,7 @@ def test_filter_low_abundance_species():
     print("Filtered DataFrame:")
     print(filtered_df.head())  
     assert "Bacteroides intestinalis" in filtered_df.index
-    assert df.loc["Bacteroides intestinalis", df.columns[0]] == 0.2203460755971263
+    assert df.loc["Bacteroides intestinalis", df.columns[0]] == 0.3542092545243491
 
 
 def test_generate_graphs():
